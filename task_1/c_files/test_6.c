850145d02f187558addeb8d49ea2e959f0fb1eef
Никита Никифоров <s02170179@practicum.cs.msu.su>
2019-09-19 13:41:05 +0300

    Writing fast programm without forks

--- task_1/c_files/test_6.c
+++ task_1/c_files/test_6.c
@@ -2,16 +2,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define CHECK
 
 #define SIZE 1024
+#define THREADS 4
+
 double c[SIZE];
 double a[SIZE * SIZE];
 double b[SIZE * SIZE];
 
 void main( int argc, char **argv) {
-
+    int block_size = SIZE * SIZE / THREADS;
     for (int i = 0; i < SIZE * SIZE; i++) { 
         b[i] = 20.19;
     }
@@ -19,7 +22,6 @@ void main( int argc, char **argv) {
     for (int i = 0; i < SIZE * SIZE; i++) {
         c[i / SIZE] += b[i];
     }
-
 
     for (int i = 0; i < SIZE * SIZE; i++) {
         a[i] = c[i / SIZE] + c[i % SIZE];
