c578a3e90975a68a932f964684fa133e9bea563c
Никита Никифоров <nickiforov.nik@gmail.com>
2019-11-27 03:33:45 +0300

    Last changes

--- task_4/fifth.cc
+++ task_4/fifth.cc
@@ -1,19 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #include <fstream>
 #include <string>
 #include "ns3/core-module.h"
@@ -27,40 +11,7 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("FifthScriptExample");
 
-// ===========================================================================
-//
-//         node 0                 node 1
-//   +----------------+    +----------------+
-//   |    ns-3 TCP    |    |    ns-3 TCP    |
-//   +----------------+    +----------------+
-//   |    10.1.1.1    |    |    10.1.1.2    |
-//   +----------------+    +----------------+
-//   | point-to-point |    | point-to-point |
-//   +----------------+    +----------------+
-//           |                     |
-//           +---------------------+
-//                5 Mbps, 2 ms
-//
-//
-// We want to look at changes in the ns-3 TCP congestion window.  We need
-// to crank up a flow and hook the CongestionWindow attribute on the socket
-// of the sender.  Normally one would use an on-off application to generate a
-// flow, but this has a couple of problems.  First, the socket of the on-off 
-// application is not created until Application Start time, so we wouldn't be 
-// able to hook the socket (now) at configuration time.  Second, even if we 
-// could arrange a call after start time, the socket is not public so we 
-// couldn't get at it.
-//
-// So, we can cook up a simple version of the on-off application that does what
-// we want.  On the plus side we don't need all of the complexity of the on-off
-// application.  On the minus side, we don't have a helper, so we have to get
-// a little more involved in the details, but this is trivial.
-//
-// So first, we create a socket and do the trace connect on it; then we pass 
-// this socket into the constructor of our simple application which we then 
-// install in the source node.
-// ===========================================================================
-//
+
 class MyApp : public Application {
 public:
     MyApp ();
@@ -139,33 +90,28 @@ void MyApp::SendPacket (void) {
 void MyApp::ScheduleTx (void) {
     if (m_running) {
         //Time tNext (Seconds (m_packetSize * 8 / static_cast<double> (m_dataRate.GetBitRate ())));
-        Time tNext(NanoSeconds(100));
+        Time tNext(NanoSeconds(10000));
         m_sendEvent = Simulator::Schedule (tNext, &MyApp::SendPacket, this);
     }
 }
-
-
-
-
-/*static void
-CwndChange (uint32_t oldCwnd, uint32_t newCwnd)
-{
-  NS_LOG_UNCOND (Simulator::Now ().GetSeconds () << "\t" << newCwnd);
-}*/
-
 
 
 static void RxDrop (std::string context, Ptr<const Packet> p) {
     NS_LOG_UNCOND (context << " RxDrop at " << Simulator::Now ().GetSeconds ());
 }
 
-static void TxBegin (std::string context, Ptr<const Packet> p) {
+/*static void TxBegin (std::string context, Ptr<const Packet> p) {
     NS_LOG_UNCOND (context << "TxBegin at " << Simulator::Now ().GetSeconds ());
+}*/
+
+static void QueDrop (std::string context, Ptr<const Packet> p) {
+    NS_LOG_UNCOND (context << " Queue Drop at " << Simulator::Now ().GetSeconds ());
 }
+
 static void MacTxDrop (std::string context, Ptr<const Packet> p) {
     NS_LOG_UNCOND (context << "MacTxDrop at " << Simulator::Now ().GetSeconds ());
 }
-uint64_t csma_num = 100;
+uint64_t csma_num = 2;
 
 int main (int argc, char *argv[]) {
     CommandLine cmd;
@@ -176,23 +122,19 @@ int main (int argc, char *argv[]) {
     
     CsmaHelper csma;
     csma.SetChannelAttribute ("DataRate", StringValue ("1Mbps"));
-    csma.SetChannelAttribute ("Delay", TimeValue (MicroSeconds (650)));
+    csma.SetChannelAttribute ("Delay", TimeValue (Seconds(1)));
+    csma.SetQueue("ns3::DropTailQueue");
 
-    NetDeviceContainer devices;
-    devices = csma.Install (nodes);
+    NetDeviceContainer devices = csma.Install (nodes);
 
-
-    //PointToPointHelper pointToPoint;
-    //pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-    //pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
-
-    //NetDeviceContainer devices;
-    //devices = pointToPoint.Install (nodes);
-
-    Ptr<RateErrorModel> em = CreateObject<RateErrorModel> ();
-    em->SetAttribute ("ErrorRate", DoubleValue (0.00001));
     for (uint32_t i = 0; i < csma_num; i++) {
-        devices.Get (i)->SetAttribute ("ReceiveErrorModel", PointerValue (em));
+        Ptr<RateErrorModel> em = CreateObject<RateErrorModel> ();
+        em->SetAttribute ("ErrorRate", DoubleValue (0.0001));
+        Ptr<DropTailQueue<Packet>> que = CreateObject<DropTailQueue<Packet>>();
+        que->SetMaxSize(QueueSize("2p"));
+        que->TraceConnect("Drop", "Host " + std::to_string(i) + ": ", MakeCallback(&QueDrop));
+        devices.Get(i)->SetAttribute("TxQueue", PointerValue(que));
+        devices.Get(i)->SetAttribute("ReceiveErrorModel", PointerValue (em));
     }
     InternetStackHelper stack;
     stack.Install (nodes);
@@ -203,30 +145,37 @@ int main (int argc, char *argv[]) {
 
     uint16_t sinkPort = 8080;
     Address sinkAddress (InetSocketAddress (interfaces.GetAddress (csma_num - 1), sinkPort));
-    PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
+    PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
     ApplicationContainer sinkApps = packetSinkHelper.Install (nodes.Get (csma_num - 1));
     sinkApps.Start (Seconds (0.));
     sinkApps.Stop (Seconds (20.));
 
 
     for (uint32_t i = 0; i < csma_num - 1; i++) {
-        Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (nodes.Get (i), TcpSocketFactory::GetTypeId ());
+        Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (nodes.Get (i), UdpSocketFactory::GetTypeId ());
         //`ns3TcpSocket->TraceConnectWithoutContext ("CongestionWindow", MakeCallback (&CwndChange));
         Ptr<MyApp> app = CreateObject<MyApp> ();
-        app->Setup (ns3TcpSocket, sinkAddress, 1040, 10000, DataRate ("10Mbps"));
+        app->Setup (ns3TcpSocket, sinkAddress, 1040, 100000, DataRate ("100Mbps"));
         nodes.Get (i)->AddApplication (app);
         app->SetStartTime (Seconds (1.));
         app->SetStopTime (Seconds (20.));
-        devices.Get (i)->TraceConnect("PhyTxBegin","Host " + std::to_string(i) + ": ", MakeCallback (&TxBegin));
+//        devices.Get (i)->TraceConnect("PhyTxBegin","Host " + std::to_string(i) + ": ", MakeCallback (&TxBegin));
         devices.Get (i)->TraceConnect("MacTxDrop","Host " + std::to_string(i) + ": ", MakeCallback (&MacTxDrop));
     }
 
     devices.Get (csma_num - 1)->TraceConnect("PhyRxDrop", "Server:" , MakeCallback (&RxDrop));
 
+    //std::ofstream file("fifth.tr", std::ios_base::binary | std::ios_base::out);
+
+    AsciiTraceHelper ascii;
+    csma.EnableAsciiAll (ascii.CreateFileStream ("fifth.tr"));
+
+
     Simulator::Stop (Seconds (20));
     Simulator::Run ();
     Simulator::Destroy ();
 
+    //file.close();
     return 0;
 }
 
